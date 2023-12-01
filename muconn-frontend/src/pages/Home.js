170a5d518df8bbc3e1332f655e613dd0461aeb94
viktoriya <victoria.dimitrova0@gmail.com>
2023-12-01 21:37:06 +0100

    display playlists from backend

--- muconn-frontend/src/pages/Home.js
+++ muconn-frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState }from 'react'
+import React, { useState, useEffect }from 'react'
 import LeftNav from "../components/LeftNav";
 import RightNav from "../components/RightNav";
 import topHits from "../assets/top-hits.png"
@@ -7,6 +7,22 @@ import {Link} from 'react-router-dom';
 
 function Home() {
   const [leftNavVisible, setLeftNavVisible] = useState(false);
+  const [userPlaylists, setUserPlaylists] = useState([]);
+
+  useEffect(() => {
+    fetchUserPlaylists();
+  }, []);
+
+  const fetchUserPlaylists = async () => {
+    try {
+      const response = await fetch('http://localhost:8080/api/playlists/user/5'); 
+      const data = await response.json();
+
+      setUserPlaylists(data);
+    } catch (error) {
+      console.error('Error fetching user playlists:', error);
+    }
+  };
 
   return (
     <div>
@@ -16,38 +32,22 @@ function Home() {
       <LeftNav visible={leftNavVisible} onClose={() => setLeftNavVisible(false)}/>
       <Link to="/login" className="login-btn">Log in</Link>
       <section>
-          <div className="section-title">
-              <h1>Muconn Playlists</h1>
-          </div>
+        <div className="section-title">
+          <h1>Muconn Playlists</h1>
+        </div>
         <div className="container-section">
-            <Link to="/playlist" className="box">
-              <img src={topHits} className="box-img"/>
-              <div className = "box-text">
-                <h2>Today's Top Hits</h2>
+          {userPlaylists.map((playlist) => (
+            <Link to={'/playlist'} className="box">
+              <img src={`http://localhost:8080/images/playlists/${playlist.image}`} className="box-img"/>
+              <div className="box-text">
+                <h2>{playlist.title}</h2>
               </div>
             </Link>
-            <Link to="/playlist" className="box">
-              <img src={topHits} className="box-img"/>
-              <div className = "box-text">
-                <h2>Today's Top Hits</h2>
-              </div>
-            </Link>
-            <Link to="/playlist" className="box">
-              <img src={topHits} className="box-img"/>
-              <div className = "box-text">
-                <h2>Today's Top Hits</h2>
-              </div>
-            </Link>
-            <Link to="/playlist" className="box">
-              <img src={topHits} className="box-img"/>
-              <div className = "box-text">
-                <h2>Today's Top Hits</h2>
-              </div>
-            </Link>
+          ))}
         </div>
       </section>
       <p className="preview-text">Sign up to get access to all features.</p>
-      {/* <RightNav></RightNav> */}
+      <RightNav></RightNav>
     </div>
   )
 }
